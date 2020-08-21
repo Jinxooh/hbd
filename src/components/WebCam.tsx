0040c52f83d3902ffb5a523e74d957ac2c08bf59
jinxooh <cjswp122@gmail.com>
2020-08-22 02:42:02 +0900

    change video quality

--- src/components/WebCam.tsx
+++ src/components/WebCam.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import Webcam, { WebcamProps } from "react-webcam";
 
 const WebCam = () =>
-  <Webcam 
+  <Webcam
     audio={true}
     forceScreenshotSourceSize={true}
     imageSmoothing={true}
     mirrored={true}
     screenshotFormat={'image/png'}
-    screenshotQuality={10}
+    screenshotQuality={1}
     minScreenshotHeight={720}
     minScreenshotWidth={720}
     onUserMedia={() => { console.log('hey')}}
