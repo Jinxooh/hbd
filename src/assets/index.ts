415721af79f3c65463c918a37e6078f76bd8d0f9
jinxooh <cjswp122@gmail.com>
2020-09-11 02:49:16 +0900

    set carousel slider

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,6 +4,14 @@ import play from './play.png';
 import pause from './pause.png';
 import close from './close.png'
 import full from './full.jpg'
+import img1 from './carousel/image1.jpg'
+import img2 from './carousel/image2.jpg'
+import img3 from './carousel/image3.jpg'
+import img4 from './carousel/image4.jpg'
+import img5 from './carousel/image5.jpg'
+import img6 from './carousel/image6.jpg'
+import img7 from './carousel/image7.jpg'
+import img8 from './carousel/image8.jpg'
 
 export {
   main,
@@ -12,4 +20,12 @@ export {
   pause,
   close,
   full,
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
 };
--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -1,12 +1,65 @@
 import React from "react";
 import { CountDown } from '../components';
 import { CountContext } from '../context';
+import styeld from 'styled-components';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import {
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+} from '../assets';
 
+const CountDownContainer = styeld.div`
+  margin-bottom: 1rem;
+`
+
+const CarouselContainer = styeld.div`
+  margin: 0 10%;
+`
 const About = () => {
   return (
-    <CountContext.Consumer>
-      {count => <CountDown count={count} />}
-    </CountContext.Consumer>
+    <>
+      <CountContext.Consumer>
+        {count =>
+        <CountDownContainer>
+          <CountDown count={count} />
+        </CountDownContainer>}
+      </CountContext.Consumer>
+      <CarouselContainer>
+        <Carousel>
+          <div>
+            <img src={img8} />
+          </div>
+          <div>
+            <img src={img1} />
+          </div>
+          <div>
+            <img src={img2} />
+          </div>
+          <div>
+            <img src={img3} />
+          </div>
+          <div>
+            <img src={img4} />
+          </div>
+          <div>
+            <img src={img5} />
+          </div>
+          <div>
+            <img src={img6} />
+          </div>
+          <div>
+            <img src={img7} />
+          </div>
+        </Carousel>
+      </CarouselContainer>
+    </>
   )
 };
 export default About;
